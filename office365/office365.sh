31cec3e601693fe573890cbc358b0d70a4363aee
sjlh_pc <myki@sjlh.com>
2019-08-20 09:59:07 +0800

    office365

---
+++ office365/office365.sh
@@ -0,0 +1,12 @@
+2019年1月22日星期二 9:11:
+office365
+开发中心
+https://developer.microsoft.com/zh-CN/office/profile/
+root@1nth.com
+Xiaofan@1myki
+#365管理
+https://portal.office.com/AdminPortal/Home#/homepage
+root@21ki.onmicrosoft.com
+Xiaofan@1myki
+2019年8月20日 09:26:17
+!X>e:+Ze@ear}+?v0r}1
